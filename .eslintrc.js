4615d9528b4f789d88f96542b8979e8f1a3ae957
James Yu Cao <caoyu19920414@gmail.com>
2024-10-03 17:34:32 -0400

    updated eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// .eslintrc.js
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off', // Disable the base rule
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
