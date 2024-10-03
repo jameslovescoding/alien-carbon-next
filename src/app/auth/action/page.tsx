0a4f0acfb2e560f2525b86571480274798df6fe6
James Yu Cao <caoyu19920414@gmail.com>
2024-10-03 12:10:31 -0400

    added testing app router and auth/action page

---
+++ src/app/auth/action/page.tsx
@@ -0,0 +1,17 @@
+import { useParams } from "next/navigation";
+
+function AuthActionPage() {
+  const { mode, oobCode, apiKey, lang } = useParams();
+
+  return (
+    <div>
+      <h1>Auth Action Page</h1>
+      <p>mode: {String(mode)}</p>
+      <p>oobCode: {String(oobCode)}</p>
+      <p>apiKey: {String(apiKey)}</p>
+      <p>lang: {String(lang)}</p>
+    </div>
+  );
+}
+
+export default AuthActionPage;
